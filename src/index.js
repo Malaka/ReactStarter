626058799c181277c99658f5d4333e9dfdd15af3
malaka <malaka.ekanayake@gmail.com>
2018-03-21 23:15:37 +1100

    prettier config added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <h1>Hey Buddy! this is great</h1>,
-  document.getElementById('app')
-);
+ReactDOM.render(<h1>Hey Buddy! this is great</h1>, document.getElementById('app'));
 
 if (module.hot) {
   module.hot.accept();
