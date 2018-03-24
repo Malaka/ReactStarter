f53182e369544b3b7e2a0756333f2386ffea1074
malaka <malaka.ekanayake@gmail.com>
2018-03-24 16:28:25 +1100

    adding game functionality

--- src/index.js
+++ src/index.js
@@ -1,20 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
 import './index.css';
 
+// TODO move this to function
 class Square extends React.Component {
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return (
+      <button className="square" onClick={this.props.onClick}>
+        {this.props.value}
+      </button>
+    );
   }
 }
 
+Square.propTypes = {
+  value: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
 class Board extends React.Component {
-  renderSquare() {
-    return <Square />;
+  constructor(props) {
+    super(props);
+    this.state = {
+      squars: Array(9).fill(null),
+      xIsNext: true,
+    };
   }
+  nextPlayer = () => (this.state.xIsNext ? 'X' : 'O');
+
+  handleClicked = i => {
+    const squars = this.state.squars.slice();
+    squars[i] = this.nextPlayer();
+    const xIsNext = !this.state.xIsNext;
+    this.setState({ squars, xIsNext });
+  };
+
+  calculateWinner = squares => {
+    const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    _.forEach(lines, line => {
+      const [a, b, c] = line;
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+      return null;
+    });
+  };
+
+  renderSquare = i => <Square value={this.state.squars[i]} onClick={() => this.handleClicked(i)} />;
 
   render() {
-    const status = 'Next player: X';
+    const winner = this.calculateWinner(this.state.squars);
+    let status;
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${this.nextPlayer()}`;
+    }
 
     return (
       <div>
